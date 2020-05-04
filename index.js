5a3859db8a2fa2a932423939bc8aa885c3e3ca31
Vinay Garg <Vinay_garg9@optum.com>
2020-05-04 09:37:00 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,6 +1,65 @@
+var noOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for(var i=0;i<7;i++){
-document.querySelector("button")[i].addEventListener("click", function()
-{ alert("i got clicked");
+for (var i = 0; i < noOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerhtml = this.innerHTML;
+    makeSound(buttonInnerhtml);
+    buttonAnimation(buttonInnerhtml);
+  });
+};
+
+// Detecting Keyoard pressed
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+  buttonAnimation(event.key);
+
 });
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      var tom_1 = new Audio("sounds/tom-1.mp3");
+      tom_1.play();
+      break;
+    case "j":
+      var tom_2 = new Audio("sounds/tom-2.mp3");
+      tom_2.play();
+      break;
+    case "k":
+      var tom_3 = new Audio("sounds/tom-3.mp3");
+      tom_3.play();
+      break;
+    case "l":
+      var tom_4 = new Audio("sounds/tom-4.mp3");
+      tom_4.play();
+      break;
+
+    default:
+
+  }
+};
+
+function buttonAnimation(key){
+  var activeButton = document.querySelector("." +key);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
 };
